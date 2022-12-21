027e3f57b3c8f435cf854376c3fb040e59b41a42
gokhantemel <gokhan.96011@gmail.com>
2022-12-21 21:18:00 +0300

    create Office Hours

---
+++ src/week9/WrapperClassMethods.java
@@ -0,0 +1,31 @@
+package week9;
+
+public class WrapperClassMethods {
+
+    public static void main(String[] args) {
+
+        // Autoboxing
+        int a = 40;
+
+        Integer b = a;
+
+        // Unboxing
+        Integer c = 50;
+
+        int d = c;
+
+        /*
+        !. parsInt():
+        2.valueOf():
+        3.toString():
+        4.Integer.max or min
+         */
+
+
+
+
+
+    }
+
+
+}
